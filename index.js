9ed148f50285a0ea3a172aceb282ac4d0005c8f4
Arthur Procópio <avprocopio@gmail.com>
2022-07-29 18:39:02 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios').default;
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      '*'
+    );
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+});
+
+app.get('/modalities', async (req, res) => {
+    const response = await axios.get('https://wine-club.herokuapp.com/modalities');
+    return res.json(response.data);
+});
+
+app.get('/modalities/:id', async (req, res) => {
+    const response = await axios.get(`https://wine-club.herokuapp.com/modalities/${req.params.id}`);
+    return res.json(response.data);
+});
+
+app.listen(80);
